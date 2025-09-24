261bf75fd70d55ea2d44e3a15688194c84459d37
vincentthelibra <vincentthelibra@gmail.com>
2025-09-24 09:45:24 +0200

    Update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,9 @@
 vim.g.autoformat = true
 
 vim.o.swapfile = false
+
+vim.g.loaded_perl_provider = 0
+
+vim.g.python3_host_prog = [[C:\Users\vincent.li\scoop\apps\python\current\python.exe]]
+
+vim.opt.shell = "C:Users\vincent.liPowerShell\pwsh.exe"
