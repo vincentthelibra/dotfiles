a55fabd65c6bc5e092149c1ab8a9f0fc030c5742
vincentthelibra <vincentthelibra@gmail.com>
2025-09-24 15:41:47 +0200

    Update autocmds.lua

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",  -- Highlight group to use
+      timeout = 200,          -- Duration in milliseconds
+    })
+  end,
+})
